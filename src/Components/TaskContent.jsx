c89d97f3ef6eed4342e0746bd176e91b31a508b2
Kate <ekaterina.shalda@gmail.com>
2022-08-04 08:31:59 +0300

    Extract inline styles to file

--- src/Components/TaskContent.jsx
+++ src/Components/TaskContent.jsx
@@ -1,32 +1,24 @@
 import TextArea from "antd/lib/input/TextArea";
 import React, { useContext } from "react";
 import { Context } from "../Context";
+import '../styles.css'
 
 export function TaskContent() {
 
-    const { date, editMode, content, updateCurrentTask} = useContext(Context);
+    const { date, editMode, content, updateCurrentTask } = useContext(Context);
 
     if (!editMode) {
-        return <div style={{
-            width: '80%',
-            display: 'inline-block',
-        }}>
+        return <div className="task-content-container">
             <div>{date}</div>
             {content}
         </div>;
     }
 
-    return <div style={{
-        width: '80%',
-        display: 'inline-block',
-    }}>
+    return <div className="task-content-container">
         <div>{date}</div>
         <TextArea
             placeholder="Type here"
-            style={{
-                width: '80%',
-                display: 'inline-block',
-            }}
+            className="task-content-container"
             value={content}
             onChange={(event) => updateCurrentTask({ content: event.target.value })}
         />
